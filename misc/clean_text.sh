03cf56f0fc681ca8c1f911128f8520aaa9fea7af
janeumanIU <janeuman@iu.edu>
2020-05-09 00:34:00 -0400

    Update files

--- misc/clean_text.sh
+++ misc/clean_text.sh
@@ -1,6 +1,13 @@
+############################################
+# Iterates through all text files in a     #
+# directory, performs the cleaning on each #
+# one, and writes to a new file prefixed   #
+# 'c_'.	     				   #
+############################################
+
 for f in *.txt
 do
-	echo "Processing $f file..."
-	cat $f | tr -d '[:punct:]' | tr -d '[:digit:]' | tr -d '[«»]' | tr -s ' ' > c_$f
+	echo "Processing file $f..."
+	cat $f | tr -d '[:punct:]' | tr -d '[:digit:]' | tr -d '[«»@#$%^*}{|£¢§]' | tr '[:upper:]' '[:lower:]' | tr '[\n\r]' ' ' | tr -s ' ' > c_$f
 done
 
--- misc/remove_stopwords.py
+++ misc/remove_stopwords.py
@@ -1,13 +1,47 @@
-import nltk
-import string
-from nltk.corpus import stopwords
+########################################
+# Remove stopwords from all text files #
+# in directory given a custom stopword #
+# list that is also a text file. All   #
+# input is obtained via the program.   #
+# The program also creates a new       #
+# output file with the prefix 'c_'     #
+# while preserving the original file.  #
+########################################
 
 import os
- 
-for file in os.listdir('/home/antspiderbee/VictorianLogic/corpus/'):
+
+#Get the stopword list path from user input.
+stopwords_file = input("Please give the path to the stopwords list file: ")
+
+#Get the corpus directory from user input.
+corpus_dir = input("Please give the directory of the corpus to be processed: ")
+
+#Get the encoding format of the corpus.
+enc_format = input("Please specify the encoding format (default with no input is UTF-8): ")
+
+#But set a default for the empty string.
+if enc_format == '':
+	enc_format = 'utf-8'
+
+#Read the stopword list into a python list without any newline characters.
+with open(stopwords_file, 'r') as s:
+	stopwords = [line.rstrip() for line in s.readlines()]
+s.close()
+
+#Iterate through the files in the given directory.
+for file in os.listdir(corpus_dir):
+
+	#First, check to see if the file is a text file.
 	if file.endswith(".txt"):
-		with open(file,'r', encoding='latin-1') as inFile, open('sw_' + file,'w+') as outFile:
-			for line in inFile.readlines():
-		    		print(" ".join([word for word in line.split() if word not in stopwords.words('english')]), file=outFile)
+		#Then, open that file using the specified encoding.
+		with open(file, 'r', encoding = enc_format) as input_file, open('c_' + file, 'w+') as output_file:
+			#And read the lines of that file into a list.
+			for line in input_file.readlines():
+				#Finally, tokenize the line, convert it back into a space-delimited string without the stopwords,
+				#and print the new line to the output file.
+				print(" ".join([word for word in line.split() if word not in stopwords]), file = output_file)
+		input_file.close()
+		output_file.close()
+	#Skip files without the text file extension.
 	else:
 		continue
